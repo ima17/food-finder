a2dcd4f748e65562ae1806f0e2f8cd4d7f715dfa
ima17 <ima.sudaraka@outlook.com>
2023-01-23 23:12:54 +0530

    Create search screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text,View,StyleSheet } from "react-native";
+
+const SearchScreen = ()=>{
+    return <Text>Search Screen</Text>
+}
+
+const style = StyleSheet.create({});
+
+export default SearchScreen;
