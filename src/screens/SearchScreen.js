bb1ce4e7b499b4399659656002a91ed2d251d29c
ima17 <ima.sudaraka@outlook.com>
2023-01-24 00:35:42 +0530

    Implement search bar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,24 @@
-import React from "react";
-import { Text,View,StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
 
-const SearchScreen = ()=>{
-    return <Text>Search Screen</Text>
-}
+const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+
+  return (
+    <View>
+      <SearchBar
+        term={term}
+        onChangeTerm={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => {
+          console.log("Submitted");
+        }}
+      />
+      <Text>Search Screen </Text>
+      <Text>{term}</Text>
+    </View>
+  );
+};
 
 const style = StyleSheet.create({});
 
