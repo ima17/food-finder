42ac5494274c221602ac0398cf1208fcf3d5b7ff
ima17 <ima.sudaraka@outlook.com>
2023-01-24 23:20:10 +0530

    Implement api search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,21 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
+import Yelp from "../api/Yelp";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [result, setResult] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await Yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      setResult(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something went wrong!");
+    }
+  };
+
+  useEffect(() => {
+    searchApi("food");
+  }, []);
 
   return (
     <View>
       <SearchBar
         term={term}
         onChangeTerm={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => {
-          console.log("Submitted");
-        }}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>Search Screen </Text>
-      <Text>{term}</Text>
+      <Text> {errorMessage ? errorMessage : null} </Text>
+      <Text>We have found {result.length} results</Text>
     </View>
   );
 };
