9df58457405d875c9d47a1247a99f3adb40c44e0
ima17 <ima.sudaraka@outlook.com>
2023-01-25 20:21:02 +0530

    Implement result list

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
+import ResultList from "../components/ResultList";
 import SearchBar from "../components/SearchBar";
 import useResult from "../hooks/useResult";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [result, errorMessage, searchApi] = useResult();
+
+  const filterResultByPrice = (price) => {
+    return result.filter((result) => {
+      return result.price === price;
+    });
+  };
 
   return (
     <View>
@@ -16,6 +23,9 @@ const SearchScreen = () => {
       />
       <Text> {errorMessage ? errorMessage : null} </Text>
       <Text>We have found {result.length} results</Text>
+      <ResultList title="Cost Effective" result={filterResultByPrice("$")} />
+      <ResultList title="Bit Pricier" result={filterResultByPrice("$$")} />
+      <ResultList title="Bit Spender" result={filterResultByPrice("$$$")} />
     </View>
   );
 };
