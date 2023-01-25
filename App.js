3d3d42395162ba633ef06a55bbb82252b4fa6ce3
ima17 <ima.sudaraka@outlook.com>
2023-01-25 23:40:31 +0530

    Create result show screen

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ResultShowScreen from "./src/screens/ResultShowScreen";
 import SearchScreen from "./src/screens/SearchScreen";
 
 const Stack = createNativeStackNavigator();
@@ -13,6 +14,7 @@ function App() {
           component={SearchScreen}
           options={{ title: "Business Search" }}
         />
+        <Stack.Screen name="ResultShow" component={ResultShowScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/ResultShowScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const ResultShowScreen = () => {
+  return (
+    <View>
+      <Text>Result Show Screen</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({});
+
+export default ResultShowScreen;
